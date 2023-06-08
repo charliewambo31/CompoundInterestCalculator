f050e260f09f12961f89a75ca41d86cde6a74ed2
charliewambo31 <111303001+charliewambo31@users.noreply.github.com>
2023-06-08 06:09:16 -0400

    code js revu

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const context=document.getElementById("data-set").getContext("2d");
+    let line =new Chart(context,{});
+
 //values from form
 const initialAmount=document.getElementById("initialamount");
 const years=document.getElementById("years");
-const rates=document.getElementById("rates ");
-const compound =document.getElementById("compound ");
+const rates=document.getElementById("rates");
+const compound =document.getElementById("compound");
 
 
 // The calculate button
@@ -13,25 +17,66 @@ button.addEventListener("click",calculateGrowth);
  const data =[];
  const labels =[];
 
+ 
  function calculateGrowth(e) {
+  
     e.preventDefault();
+    
+
+    data.length = 0;
+    labels.length = 0;
+    let growth = 0 ;
 
     try{
-        const initial=parseInt(initialAmount.value);
-        const period=parseInt(years.value);
-        const interest=parseInt(rates.value);
-        const comp=parseInt(compound.value)
+        
+        const initial = parseInt(initialAmount.value);
+        const period = parseInt(years.value);
+        const interest = parseInt(rates.value);
+        const comp = parseInt(compound.value)
 
-        for(let i=1 ;i<=period;i++){
-            const final=initial * Math.pow(1 + ((interest / 100)/comp),comp*i);
-            data.push(final);
+        for(let i = 1 ;i <= period;i++){
+            let final;
+            final = initial * Math.pow(1 + ((interest / 100)/comp),comp*i);
+           //final = initial*interest*i;
+            data.push(toDecimal(final,2)) ;
             labels.push("Year"+ i);
+            growth = toDecimal(final,2) ; 
         }
+        // console.table(data);
+        message.innerText = `Vous aurez un montant de ${growth} apres ${period} periodes`;
+        drawGraph();
      
 
     }catch (error){
+        
+        console.error("error");
+    }
 
     }
+ 
+ console.log("deloo");
+
+ function drawGraph(){
+    line.destroy()
+    line = new Chart (context,{
+        type:"line",
+        data:{
+            labels,
+            datasets:[{
+                label:"compound",
+                data,
+                fill: true,
+                background:"rgba(12,141,0,0.7)",
+                borderWitdh: 3
+            }]
+            
+        }
+    });
+ }
+ console.log("deloo");
+ function toDecimal(value,decimals){
+    return +value.toFixed(decimals);
  }
 
-
+ 
+});
